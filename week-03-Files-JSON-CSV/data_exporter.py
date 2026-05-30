05fd2ac5674936a798aea001791cf8796f513b99
Hugonofre <hugoant7@gmail.com>
2026-05-29 22:43:12 -0600

    feat: add exportar_json function to data_exporter

---
+++ week-03-Files-JSON-CSV/data_exporter.py
@@ -0,0 +1,12 @@
+import json
+
+def exportar_json(datos, filepath):
+    with open(filepath, 'w') as f:
+        json.dump(datos, f, indent=4)
+datos = [
+    {'service': 'nginx', 'level': 'INFO', 'status': 'activo'},
+    {'service': 'mysql', 'level': 'ERROR', 'status': 'error'}
+]
+
+exportar_json(datos, "reporte.json")
+print("Archivo JSON exportado.")
