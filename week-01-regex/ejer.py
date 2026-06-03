c1f069a8143dc8b7db7d335a2cb0bb371174bbac
Hugonofre <hugoant7@gmail.com>
2026-06-02 22:32:38 -0600

    chore: add week-05 project structure

---
+++ week-01-regex/ejer.py
@@ -0,0 +1,23 @@
+import re
+
+def parse_log_line(line):
+    pattern = r'(\d{4}-\d(2)-\d(2) \d{2}:\d{2}:\d{2}) (ERROR|WARN|INFO|DEBUG) \[(.+?)\] (.+)'
+    match = re.match(pattern, line)
+    
+    if match:
+        return {
+            'timestamp': match.group(1),
+            'level': match.group(2),
+            'service': match.group(3),
+            'message': match.group(4),
+        }
+    return None
+
+line_ok = "2024-11-15 14:30:35 INFO [web-service] Service started successfully"
+line_bad = "Invalid log entry without proper formay"
+
+print(parse_log_line(line_ok))
+print(parse_log_line(line_bad))
+
+      
+    
