3f227444a5ef5d8aead20f4677cd57b1d5ee617f
Hugonofre <hugoant7@gmail.com>
2026-06-05 23:26:53 -0600

    chore: create week 7 project structure

---
+++ week-07-api-status/verificar_url.py
@@ -0,0 +1,11 @@
+import requests
+
+def verificar_url(url):
+    """
+    Hace una petición GET a la URL.
+    Retorna un diccionario con:
+    - url
+    - status_code
+    - disponible (True si status_code == 200)
+    """
+    pass
